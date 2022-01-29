138d0fb0322f2cc9b54228721dc5e19038bf414d
Jose Antonio Marín <jamarin@quis.es>
2022-01-29 17:54:07 +0100

    Add some changes for production

--- bot.js
+++ bot.js
@@ -1,6 +1,11 @@
 require('dotenv').config()
 const { Telegraf } = require('telegraf')
-const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN, {webhook: {port: process.env.PORT}})
+const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN, )
+
+if(process.env.NODE_ENV === 'production') {
+    bot.telegram.setWebhook(`${process.env.URL}/bot${process.env.TELEGRAM_BOT_TOKEN}`)
+    bot.startWebhook(`/bot${process.env.TELEGRAM_BOT_TOKEN}`, null, process.env.PORT)
+}
 
 bot.launch().then(async () => {
 
