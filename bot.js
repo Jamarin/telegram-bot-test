5a4f45e1b3a4fa75ac91547c7521d2a11fd7f04b
Jose Antonio Marín <jamarin@quis.es>
2022-01-29 17:44:12 +0100

    Add port

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const { Telegraf } = require('telegraf')
-const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN)
+const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN, {webhook: {port: process.env.PORT}})
 
 bot.launch().then(async () => {
 
