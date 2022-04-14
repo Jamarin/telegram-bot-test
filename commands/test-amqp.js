bc48bdfc4415a70e4a250f6986b847edb4ab5724
Jose Antonio Marín <jamarin@quis.es>
2022-04-14 11:25:09 +0200

    Add some amqp

--- commands/test-amqp.js
+++ commands/test-amqp.js
@@ -1,34 +1,25 @@
+const { publish, listen } = require('../helpers/amqp-helper');
+
 const command = async (ctx) => {
-    require('amqplib/callback_api')
-        .connect('amqps://nhiqhejo:DzzAKXV_2WC3zIeSS_RAtOtgICGpX7Ek@squid.rmq.cloudamqp.com/nhiqhejo', function(err, conn) {
-            if (err != null) console.error(err);
-            publisher(conn);
-            listener(conn);
-        });
-}
-
-const publisher = (conn) => {
-    conn.createChannel((err, ch) => {
-        if (err != null) console.error(err);
-        ch.assertQueue('games-api');
-        const message = {
-            game: {
-                id: 1
-            }
-        };
-        ch.sendToQueue('games-api', Buffer.from(JSON.stringify(message)));
-    })
-}
-
-const listener = (conn) => {
-    conn.createChannel((err, ch) => {
-        if (err != null) console.error(err);
-        ch.assertQueue('games-front');
-        ch.consume('games-front', (msg) => {
-            console.log(msg.content.toString());
-            ch.ack(msg);
-        });
-    })
+    const message = {
+        type: 'get-game',
+        game: {
+            id: 1
+        },
+        from: {
+            id: ctx.from.id,
+            username: ctx.from.username
+        }
+    };
+    const queue = 'games-api';
+    publish(message, queue);
+    const response = listen()
+    console.log(response);
+    if(response !== null) {
+        ctx.reply(`El juego solicitado es ${response.game.name}`);
+    } else {
+        ctx.reply(`Waiting...`);
+    }
 }
 
 module.exports = command
